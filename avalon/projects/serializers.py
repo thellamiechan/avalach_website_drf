30174a467c6f27c370d1455b125fd4be7d3c191f
JP <133753350+thellamiechan@users.noreply.github.com>
2024-11-20 13:12:06 +1000

    added owner permissions

--- avalon/projects/serializers.py
+++ avalon/projects/serializers.py
@@ -7,3 +7,15 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = apps.get_model('projects.Project')
         fields ='__all__'
+
+class ProjectDetailSerializer(ProjectSerializer):
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.synopsis = validated_data.get('synopsis', instance.synopsis)
+        instance.image = validated_data.get('image', instance.image)
+        instance.price = validated_data.get('price', instance.price)
+        instance.owner = validated_data.get('owner', instance.owner)
+        instance.save()
+        return instance
--- avalon/projects/views.py
+++ avalon/projects/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, ProjectDetailSerializer
 from django.http import Http404
 from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
@@ -29,10 +30,13 @@ class ProjectList(APIView):
         )
         
 class ProjectDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+           project = Project.objects.get(pk=pk)
+           self.check_object_permissions(self.request, project)
+           return project
         except Project.DoesNotExist:
             raise Http404
     
@@ -40,3 +44,13 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectSerializer(project)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
